89214338652c79f39c65caad496328f1bc7f63b7
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-30 22:00:54 +0545

    added responsivense for mobile devices

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nav = document.querySelector('nav');
 const navbarmenuBtn = document.querySelector('.nav-menu-btn');
 const navbarForm= document.querySelector('.navbar-form');
 const navbarformclsbtn= document.querySelector('.navbar-form-close');
-const navbarsearchbtn=document.querySelector('.nabar-search-btn');
+const navbarsearchbtn=document.querySelector('.navbar-search-btn');
 
 function navisactive(){
     header.classList.toggle('active');
@@ -14,3 +14,6 @@ function navisactive(){
 
 navbarmenuBtn.addEventListener('click', navisactive);
 
+const searchbarISactive = () => navbarForm.classList.toggle('active');
+navbarsearchbtn.addEventListener('click', searchbarISactive);
+navbarformclsbtn.addEventListener('click', searchbarISactive);
